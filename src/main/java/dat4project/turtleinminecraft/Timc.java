9419cc38fa353ade5e28c2583c490e94bc26cce1
BenjaminCB <benjamincb@hotmail.dk>
2022-02-04 14:47:24 +0100

    add custom block that can be placed, though textures do not work

--- src/main/java/dat4project/turtleinminecraft/Timc.java
+++ src/main/java/dat4project/turtleinminecraft/Timc.java
@@ -1,8 +1,14 @@
 package dat4project.turtleinminecraft;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.fabricmc.fabric.api.tool.attribute.v1.FabricToolTags;
+import net.minecraft.block.Block;
+import net.minecraft.block.Material;
 import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.sound.BlockSoundGroup;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import net.minecraft.util.Identifier;
@@ -14,7 +20,12 @@ public class Timc implements ModInitializer {
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger("timc");
 
-	public static final Item GraphicsTurtle = new Item(new Item.Settings().group(ItemGroup.MISC));
+	public static final Item GraphicsTurtleItem = new Item(new Item.Settings().group(ItemGroup.MISC));
+
+	public static final Block GraphicsTurtleBlock = new Block(FabricBlockSettings.of(Material.METAL)
+																				 .strength(5.0F, 6.0F)
+																				 .sounds(BlockSoundGroup.METAL)
+			                                                                     .breakByTool(FabricToolTags.PICKAXES));
 
 	@Override
 	public void onInitialize() {
@@ -22,7 +33,9 @@ public class Timc implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		Registry.register(Registry.ITEM, new Identifier("timc", "graphics_turtle"), GraphicsTurtle);
+		Registry.register(Registry.ITEM, new Identifier("timc", "graphics_turtle_item"), GraphicsTurtleItem);
+		Registry.register(Registry.BLOCK, new Identifier("timc", "graphics_turtle_block"), GraphicsTurtleBlock);
+		Registry.register(Registry.ITEM, new Identifier("timc", "graphics_turtle_block"), new BlockItem(GraphicsTurtleBlock, new Item.Settings().group(ItemGroup.MISC)));
 
 		LOGGER.info("Hello Fabric world!");
 	}
