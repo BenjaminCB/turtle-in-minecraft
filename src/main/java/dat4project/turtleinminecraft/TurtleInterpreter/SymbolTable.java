1a892d17050b988273ddbbfc2b38c90115d04d88
BenjaminCB <benjamincb@hotmail.dk>
2022-04-22 18:56:06 +0200

    second draft only which only saves symbols in current call stack

--- src/main/java/dat4project/turtleinminecraft/TurtleInterpreter/SymbolTable.java
+++ src/main/java/dat4project/turtleinminecraft/TurtleInterpreter/SymbolTable.java
@@ -1,54 +1,39 @@
 package dat4project.turtleinminecraft.TurtleInterpreter;
 
 import java.util.Deque;
+import java.util.ArrayDeque;
 import java.util.HashMap;
 import java.util.Map;
 
 public class SymbolTable<T> {
-    private SymbolTable parent;
-    private Map<String, SymbolTable<T>> children;
-    private Map<String, T> symbols;
-
-    private SymbolTable(SymbolTable parent) {
-        this.parent = parent;
-        children = new HashMap<String, SymbolTable<T>>();
-        symbols = new HashMap<String, T>();
-    }
+    private Deque<Map<String, T>> tables;
 
     public SymbolTable() {
-        this(null);
+        tables = new ArrayDeque<>();
+        tables.push(new HashMap<>());
     }
 
-    public void put(String name, T val, Deque<String> scope) {
-        if (scope.isEmpty()) {
-            // traversed to the correct scope
-            symbols.put(name, val);
-        } else {
-            // continue traversing
-            String nextScope = scope.pollLast();
-            if (children.containsKey(nextScope)) {
-                // already have a symbol table for this scope
-                children.get(nextScope).put(name, val, scope);
-            } else {
-                // create table for new scope
-                SymbolTable t = new SymbolTable<T>(this);
-                children.put(nextScope, t);
-                t.put(name, val, scope);
-            }
-        }
+    public void enterScope() {
+        tables.push(new HashMap<>());
     }
 
-    public T get(String name, Deque<String> scope) {
+    public void exitScope() {
+        tables.pop();
+    }
+
+    public void put(String name, T val) {
+        tables.peek().put(name, val);
+    }
+
+    public T get(String name) {
         T res = null;
 
-        // first check deeper symbol tables
-        String nextScope = scope.pollLast();
-        if (children.containsKey(nextScope)) {
-            res = children.get(nextScope).get(name, scope);
+        for (Map<String, T> table : tables) {
+            if (table.containsKey(name)) {
+                res = table.get(name);
+                break;
+            }
         }
-
-        // if nothing was found check current table
-        res = res == null ? symbols.get(name) : res;
 
         return res;
     }
