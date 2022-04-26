1d29e0f03768acd7ebcf9bc23a1b197eeb2ab978
nikolajrk <nikolajrk@gmail.com>
2022-04-26 12:31:40 +0200

    Implemented equal for arrays

--- src/main/java/dat4project/turtleinminecraft/TurtleInterpreter/ArrayVal.java
+++ src/main/java/dat4project/turtleinminecraft/TurtleInterpreter/ArrayVal.java
@@ -33,6 +33,24 @@ public class ArrayVal extends TimcVal {
 
     @Override
     protected boolean timcValEquals(TimcVal o) {
-        return false;
+        if(o instanceof ArrayVal arr){
+            int a = arr.val.size();
+            if(a == this.val.size()){
+
+                for (int i = 0; i < a; i++) {
+                    if(arr.val.get(i) != this.val.get(i)) return false;
+                }
+
+            } else {   
+                System.exit(0);
+                return false;
+            }
+
+            
+        }else {   
+            System.exit(0);
+            return false;
+        }
+        return true;
     }
 }
