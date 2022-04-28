34201b88433efcdd21edba4ae3d964678fc139fc
Andreas Tor Mortensen <andr997c@gmail.com>
2022-04-28 16:08:11 +0200

    Moved command block logic into entity + redstone activation

--- src/main/java/dat4project/turtleinminecraft/TurtleCommandBlock.java
+++ src/main/java/dat4project/turtleinminecraft/TurtleCommandBlock.java
@@ -24,14 +24,9 @@ public class TurtleCommandBlock extends BlockWithEntity{
     public static final DirectionProperty FACING = Properties.FACING;
     public static final Identifier ID = new Identifier("timc", "command_turtle_block");
 
-    private BlockPos turtlePos;
-    private BlockState turtleBlockState;
-    private Direction turtleDirection;
-
     public TurtleCommandBlock(Settings settings) {
         super(settings);
         this.setDefaultState((BlockState)((BlockState)((BlockState)this.stateManager.getDefaultState()).with(FACING, Direction.NORTH)));
-        this.turtleBlockState = Timc.GraphicsTurtleBlock.getDefaultState();
     }
     public BlockState getPlacementState(ItemPlacementContext ctx) {
         return this.getDefaultState().with(FACING, ctx.getPlayerFacing());
@@ -60,61 +55,7 @@ public class TurtleCommandBlock extends BlockWithEntity{
 	public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
 		// You need a Block.createScreenHandlerFactory implementation that delegates to the block entity,
 		// such as the one from BlockWithEntity
-        if(!world.isClient) {
-            BlockState currentBlockState = world.getBlockState(turtlePos);
-            if(!currentBlockState.equals(state)) {
-                world.removeBlock(turtlePos, true);
-            }
-            updateTurtlePos();
-            world.setBlockState(turtlePos, turtleBlockState);
-        }
-		//player.openHandledScreen(state.createScreenHandlerFactory(world, pos));
+		player.openHandledScreen(state.createScreenHandlerFactory(world, pos));
 		return ActionResult.SUCCESS;
 	}
-
-    @Override
-    public void onSteppedOn(World world, BlockPos pos, BlockState state, Entity entity) {
-        if(!world.isClient) {
-            turtleDirection = turtleDirection.rotateYClockwise();
-            turtleBlockState = turtleBlockState.with(FACING, turtleDirection);
-            if(!world.getBlockState(turtlePos).equals(state)) {
-                world.setBlockState(turtlePos, turtleBlockState);
-            }
-            Timc.LOGGER.info(turtleDirection.asString());
-        }
-    }
-    
-    @Override
-    public void onPlaced(World world, BlockPos pos, BlockState state, LivingEntity placer, ItemStack itemStack) {
-        if(!world.isClient) {
-            turtleDirection = state.get(FACING);
-            turtleBlockState = turtleBlockState.with(FACING, turtleDirection);
-            turtlePos = pos;
-        }
-    }
-
-    private void updateTurtlePos() {
-        switch(turtleDirection) {
-            case DOWN:
-                turtlePos = turtlePos.down();
-                break;
-            case EAST:
-                turtlePos = turtlePos.east();
-                break;
-            case NORTH:
-                turtlePos = turtlePos.north();
-                break;
-            case SOUTH:
-                turtlePos = turtlePos.south();
-                break;
-            case UP:
-                turtlePos = turtlePos.up();
-                break;
-            case WEST:
-                turtlePos = turtlePos.west();
-                break;
-            default:
-                break;
-        }
-    }
 }
