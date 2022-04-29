6aa5b7fbb733ca042a8d3af2abd0ba3b0cc3fd5c
KittenSlayerExpress <hejsa2574@gmail.com>
2022-04-29 11:41:30 +0200

    Added look for absolute and relative directions

--- src/main/java/dat4project/turtleinminecraft/TurtleCommandBlockEntity.java
+++ src/main/java/dat4project/turtleinminecraft/TurtleCommandBlockEntity.java
@@ -1,5 +1,9 @@
 package dat4project.turtleinminecraft;
 
+import dat4project.turtleinminecraft.TurtleInterpreter.AbsDirVal;
+import dat4project.turtleinminecraft.TurtleInterpreter.BlockVal;
+import dat4project.turtleinminecraft.TurtleInterpreter.RelDirVal;
+import net.fabricmc.fabric.api.event.client.ClientSpriteRegistryCallback;
 import net.minecraft.block.Block;
 import org.jetbrains.annotations.Nullable;
 import net.minecraft.block.BlockState;
@@ -18,6 +22,8 @@ import net.minecraft.util.collection.DefaultedList;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
+
+import java.util.Objects;
 
 public class TurtleCommandBlockEntity extends BlockEntity implements NamedScreenHandlerFactory, ImplementedInventory {
     private final DefaultedList<ItemStack> inventory = DefaultedList.ofSize(1, ItemStack.EMPTY);
@@ -74,11 +80,28 @@ public class TurtleCommandBlockEntity extends BlockEntity implements NamedScreen
         }
         world.setBlockState(entity.turtlePos, entity.turtleState);
     }
-    public static Block look(World world, TurtleCommandBlockEntity entity, @Nullable Direction direction){
-        if(!(direction == null)){
-            entity.turtleDirection = direction;
+    public static Block look(World world, TurtleCommandBlockEntity entity, AbsDirVal.AbsDir direction){
+        Direction mcDirection = entity.turtleDirection;
+        switch (direction){
+            case EAST -> mcDirection = Direction.EAST;
+            case WEST -> mcDirection = Direction.WEST;
+            case NORTH -> mcDirection = Direction.NORTH;
+            case SOUTH -> mcDirection = Direction.SOUTH;
         }
+
+        entity.turtleDirection = mcDirection;
+
         return world.getBlockState(entity.turtlePos.add(entity.turtleDirection.getVector())).getBlock();
+    }
+    public static Block look(World world, TurtleCommandBlockEntity entity, RelDirVal.RelDir relDir){
+        return switch (relDir) {
+            case UP -> (world.getBlockState(entity.turtlePos.add(Direction.UP.getVector())).getBlock());
+            case DOWN -> (world.getBlockState(entity.turtlePos.add(Direction.DOWN.getVector())).getBlock());
+            case LEFT -> (world.getBlockState(entity.turtlePos.add(entity.turtleDirection.rotateYCounterclockwise().getVector())).getBlock());
+            case RIGHT -> (world.getBlockState(entity.turtlePos.add(entity.turtleDirection.rotateYClockwise().getVector())).getBlock());
+            case FRONT -> world.getBlockState(entity.turtlePos.add(entity.turtleDirection.getVector())).getBlock();
+            case BACK -> world.getBlockState(entity.turtlePos.add(entity.turtleDirection.getOpposite().getVector())).getBlock();
+        };
     }
 
 
