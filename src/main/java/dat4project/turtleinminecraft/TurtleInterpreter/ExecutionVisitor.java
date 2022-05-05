cbfbef4c42d6ee9ddfc3e0e2ec74a0fbcf72fd43
Pederbuus <peterbuussteffensen@gmail.com>
2022-05-05 11:30:45 +0200

    Build-in functions now has a 'nothing' return
    This now creates an error:
    
    OR = 1 == down()

--- src/main/java/dat4project/turtleinminecraft/TurtleInterpreter/ExecutionVisitor.java
+++ src/main/java/dat4project/turtleinminecraft/TurtleInterpreter/ExecutionVisitor.java
@@ -598,7 +598,7 @@ public class ExecutionVisitor extends timcBaseVisitor<TimcVal> {
             tcbEntity.move(dir);
         }
 
-        return null; 
+        return new NothingVal();
     }
 
     @Override public TimcVal visitBackwardFunc(timcParser.BackwardFuncContext ctx) { 
@@ -619,7 +619,7 @@ public class ExecutionVisitor extends timcBaseVisitor<TimcVal> {
             tcbEntity.move(dir);
         }
 
-        return null; 
+        return new NothingVal();
     }
 
     @Override public TimcVal visitUpFunc(timcParser.UpFuncContext ctx) { 
@@ -640,7 +640,7 @@ public class ExecutionVisitor extends timcBaseVisitor<TimcVal> {
             tcbEntity.move(dir);
         }
 
-        return null; 
+        return new NothingVal();
     }
 
     @Override public TimcVal visitDownFunc(timcParser.DownFuncContext ctx) { 
@@ -661,7 +661,7 @@ public class ExecutionVisitor extends timcBaseVisitor<TimcVal> {
             tcbEntity.move(dir);
         }
 
-        return null; 
+        return new NothingVal();
     }
 
     @Override public TimcVal visitLookFunc(timcParser.LookFuncContext ctx) {
